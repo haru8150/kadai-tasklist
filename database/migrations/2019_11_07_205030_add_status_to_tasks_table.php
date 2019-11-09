f48417deef694fe3237e7e8cad7db5c6b260489f
haruki.takahashi <harukaze8150@gmail.com>
2019-11-09 08:08:11 +0000

    migration file fix

--- database/migrations/2019_11_07_205030_add_status_to_tasks_table.php
+++ database/migrations/2019_11_07_205030_add_status_to_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStatusToTasksTable extends Migration
+class AddStatusToTasksTable_test extends Migration
 {
     /**
      * Run the migrations.
@@ -26,7 +26,7 @@ class AddStatusToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            //
+             $table->dropColumn('status');
         });
     }
 }
